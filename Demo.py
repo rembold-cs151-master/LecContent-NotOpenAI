d9ac130be3636cf3a3fb463f6a69b24e108699db
Jed Rembold <jedediah.r@gmail.com>
2026-04-20 01:39:51 -0700

    Add initial content

---
+++ Demo.py
@@ -0,0 +1,1 @@
+from notopenai import NotOpenAI
---
+++ Practice.py
@@ -0,0 +1,26 @@
+from notopenai import NotOpenAI
+import json
+
+def lookup():
+    """Uses the NotOpenAI API to look up the 5 most influential adventure games
+    and saves the returned JSON data to disk.
+    """
+
+
+def process_json(filename):
+    """Reads in a JSON file to print to the screen all the contained adventure
+    games which are graphical in nature.
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    lookup()
+    
+    # Uncomment the below line AND COMMENT THE ABOVE lookup() when ready
+    # If you don't comment the above, you'll regenerate the file and 
+    # the structure may change
+
+    # process_json(your_filename) 
